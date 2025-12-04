5590a6cf76c9f6299060611dfa4577d41511b66d
gokselk <gokselk@users.noreply.github.com>
2025-12-05 00:11:41 +0300

    Rm flakehub cache

--- modules/nixos/base/nix-settings.nix
+++ modules/nixos/base/nix-settings.nix
@@ -16,12 +16,10 @@
       substituters = [
         "https://cache.nixos.org"
         "https://nix-community.cachix.org"
-        "https://cache.flakehub.com"
       ];
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "cache.flakehub.com-1:t6986ugxCA+d/ZF9IeMzJkyqi5mDhvFIx7KA/ipulzE="
       ];
     };
 
