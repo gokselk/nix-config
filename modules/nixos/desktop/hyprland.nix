bdf02c5e33f7205283adcda5bca88a675f309a2f
gokselk <gokselk.dev@gmail.com>
2025-12-16 01:35:52 +0300

    package name change

--- modules/nixos/desktop/hyprland.nix
+++ modules/nixos/desktop/hyprland.nix
@@ -38,7 +38,7 @@
     packages = with pkgs; [
       noto-fonts
       noto-fonts-cjk-sans
-      noto-fonts-emoji
+      noto-fonts-color-emoji
       nerd-fonts.jetbrains-mono
       nerd-fonts.fira-code
     ];
