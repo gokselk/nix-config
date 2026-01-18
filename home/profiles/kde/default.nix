b3b840f9199421e037f9bdcf5dc5c1c08d72b3a4
gokselk <gokselk.dev@gmail.com>
2026-01-18 17:31:30 +0300

    Add Catppuccin theming to KDE profile

--- home/profiles/kde/default.nix
+++ home/profiles/kde/default.nix
@@ -1,4 +1,30 @@
 # KDE Plasma desktop profile
+# Catppuccin theming for Plasma, cursors, and terminal
 { config, pkgs, lib, ... }:
 {
+  # Terminal: ghostty
+  programs.ghostty = {
+    enable = true;
+    settings = {
+      font-family = "JetBrainsMono Nerd Font";
+      font-size = 11;
+      window-padding-x = 10;
+      window-padding-y = 10;
+    };
+  };
+
+  # Catppuccin theming
+  catppuccin = {
+    flavor = "mocha";
+    accent = "blue";
+    plasma.enable = true;
+    ghostty.enable = true;
+  };
+
+  # Cursor theme
+  home.pointerCursor = {
+    name = "catppuccin-mocha-dark-cursors";
+    package = pkgs.catppuccin-cursors.mochaDark;
+    size = 24;
+  };
 }
