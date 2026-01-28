907fd7ac73a100f094223ac48c3275d9f5bdaf53
gokselk <gokselk.dev@gmail.com>
2026-01-28 21:52:06 +0300

    Hide systemd-boot menu + plymouth.use-simpledrm for smoother boot

--- modules/nixos/core/boot.nix
+++ modules/nixos/core/boot.nix
@@ -8,8 +8,11 @@
   # Limit number of generations to keep
   boot.loader.systemd-boot.configurationLimit = 10;
 
+  # Hide systemd-boot menu (hold key to show)
+  boot.loader.timeout = 0;
+
   # Quiet boot (suppress messages before Plymouth)
-  boot.kernelParams = [ "quiet" "splash" ];
+  boot.kernelParams = [ "quiet" "splash" "plymouth.use-simpledrm" ];
   boot.consoleLogLevel = 0;
   boot.initrd.verbose = false;
 }
